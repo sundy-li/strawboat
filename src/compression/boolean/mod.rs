35f0447ac970fef9c1d6f737372232f3acb482a4
sundyli <543950155@qq.com>
2023-07-16 22:10:28 +0800

    fix sample bug

--- src/compression/boolean/mod.rs
+++ src/compression/boolean/mod.rs
@@ -235,7 +235,7 @@ fn compress_sample_ratio<C: BooleanCompression>(
 ) -> f64 {
     let mut rng = thread_rng();
 
-    let stats = if sample_count * sample_size >= stats.src.len() {
+    let stats = if stats.src.len() / sample_count <= sample_size {
         stats.clone()
     } else {
         let array = &stats.src;
